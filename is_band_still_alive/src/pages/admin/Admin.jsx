ef6ccb57ba80a81de1d95afbbf243ec76e255df9
Dimags500 <73039506+Dimags500@users.noreply.github.com>
2022-06-04 11:34:45 +0300

    init commit

---
+++ is_band_still_alive/src/pages/admin/Admin.jsx
@@ -0,0 +1,18 @@
+import react  , { useState ,useEffect } from "react"
+
+
+
+
+export const Admin = () =>{
+
+
+    return(
+        <div>
+            <div>
+                <button>Heavy-Metal</button>
+                <button>Rock</button>
+            </div>
+
+        </div>
+    );
+}
