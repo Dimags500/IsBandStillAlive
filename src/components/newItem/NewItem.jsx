582df7d03ba68c952c1d1f3f4f708fcbe579559a
Dimags500 <73039506+Dimags500@users.noreply.github.com>
2022-06-07 15:38:36 +0300

    search added

--- src/components/newItem/NewItem.jsx
+++ src/components/newItem/NewItem.jsx
@@ -1,4 +1,6 @@
 import react  , { useState ,useEffect , useRef} from "react"
+import {getYearsByName1 ,getYearsByName2} from '../../services/api//requests' 
+import'./newitem.css'
 
 
 export const NewItem = (props) =>{
@@ -9,6 +11,8 @@ export const NewItem = (props) =>{
     const name  = useRef(item.name);
     const start  = useRef(item.start);
     const end= useRef(item.end);
+    const activeYears = useRef('text');
+
 
 
 
@@ -37,15 +41,38 @@ function isStringCheck (input){
 
     return true
 }
+
+const getYears = async (source)=> {
+    if(source === 1){
+        activeYears.current.value = await getYearsByName1(item.name)
+    }
+    if(source === 2){
+        activeYears.current.value = await getYearsByName2(item.name)
+
+    }
+}
     return(
-        <div>
+        <div className="new-item">
             <div>
                 <input type="text" placeholder="name"  onChange={()=>  {  if(isStringCheck(name.current.value))  setValues()   }}  value={item.name} ref={name}  />
                 <input type="text" placeholder="start year" onChange={()=> {  if(isNmberCheck(name.current.value))  setValues() }}  value={item.start} ref={start}     />
                 <input type="text" placeholder="end year" onChange={()=> {  if(isNmberCheck(name.current.value))  setValues() }}  value={item.end}  ref={end}        />
                 <button onClick={()=> props.callback(item)}  >submit</button>
 
+
+         
+
             </div>
+
+            <div className="get-years">
+                            <textarea ref={activeYears} name="" id="" >{activeYears.current.value}</textarea>
+            <button  onClick={ () => {getYears(1)}}> metal-archives </button>
+            <button  onClick={ () => {getYears(2)}}> last.fm </button>
+            </div>
+
+
+
+          
         </div>
     );
 }
